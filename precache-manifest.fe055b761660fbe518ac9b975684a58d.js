dc7fed7d31ce425c2c56e69993c9bfe3493b2244
Fsss126 <32033964+Fsss126@users.noreply.github.com>
2019-08-08 19:51:28 +0300

    New build

---
+++ precache-manifest.fe055b761660fbe518ac9b975684a58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "776a913da3c22542082d",
+    "url": "/geo-editor/static/js/runtime~main.776a913d.js"
+  },
+  {
+    "revision": "88171cf2e36c613566ce",
+    "url": "/geo-editor/static/js/main.88171cf2.chunk.js"
+  },
+  {
+    "revision": "8c53e55406b62e22d826",
+    "url": "/geo-editor/static/js/1.8c53e554.chunk.js"
+  },
+  {
+    "revision": "88171cf2e36c613566ce",
+    "url": "/geo-editor/static/css/main.c344beae.chunk.css"
+  },
+  {
+    "revision": "8c53e55406b62e22d826",
+    "url": "/geo-editor/static/css/1.39b2f2b4.chunk.css"
+  },
+  {
+    "revision": "a964a40b64ba97935f6c2409f44086e0",
+    "url": "/geo-editor/index.html"
+  }
+];

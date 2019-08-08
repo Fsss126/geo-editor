94bc3fb96bfc7171fd3ec4641a2beefeb8ea51a2
Fsss126 <32033964+Fsss126@users.noreply.github.com>
2019-08-08 19:51:28 +0300

    New build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6d6531566c77b326c0f553c0a00d9af3.js"
+  "/geo-editor/precache-manifest.fe055b761660fbe518ac9b975684a58d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/geo-editor/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
